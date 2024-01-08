6eca582c840832ea7b955f8d810ccd21c88a32f6
Paulesh5 <paulesh5@gmail.com>
2024-01-08 16:36:57 -0500

    first commit

---
+++ src/routers/veterinario_routes.js
@@ -0,0 +1,37 @@
+import {Router} from 'express'
+
+//Crear una instancia de Router
+const router = Router()
+
+// Importar los metodos del controlador
+import {
+    login,
+    perfil,
+    registro,
+    confirmEmail,
+    listarVeterinarios,
+    detalleVeterinario,
+    actualizarPerfil,
+    actualizarPassword,
+    recuperarPassword,
+    comprobarTokenPasword,
+    nuevoPassword,
+} from "../controllers/veterinario_controller.js";
+
+// Definir las rutas
+
+router.post("/login", login);
+router.post("/registro", registro);
+router.get("/confirmar/:token", confirmEmail);
+router.get("/veterinarios", listarVeterinarios);
+router.get("/recuperar-password", recuperarPassword);
+router.get("/recuperar-password/:token", comprobarTokenPasword);
+router.post("/nuevo-password/:token", nuevoPassword);
+
+router.get("/perfil", perfil);
+router.put('/veterinario/actualizarpassword',actualizarPassword)
+router.get("/veterinario/:id", detalleVeterinario);
+router.put("/veterinario/:id", actualizarPerfil);
+
+
+export default router
